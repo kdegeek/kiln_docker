3f4858973a62bd9ba15d593ef55c002bf3481e28
scosman <scosman@users.noreply.github.com>
2025-03-14 11:56:15 -0400

    Fix import

--- libs/core/kiln_ai/adapters/model_adapters/__init__.py
+++ libs/core/kiln_ai/adapters/model_adapters/__init__.py
@@ -7,10 +7,10 @@ Model adapters are used to call AI models, like Ollama, OpenAI, etc.
 
 from . import (
     base_adapter,
-    openai_model_adapter,
+    litellm_adapter,
 )
 
 __all__ = [
     "base_adapter",
-    "openai_model_adapter",
+    "litellm_adapter",
 ]
