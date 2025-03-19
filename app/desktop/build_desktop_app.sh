96812c7105d1e9547c321a2f825b720606c6009f
scosman <scosman@users.noreply.github.com>
2025-03-19 11:54:05 -0400

    Build script needs hidden import

--- app/desktop/build_desktop_app.sh
+++ app/desktop/build_desktop_app.sh
@@ -66,5 +66,5 @@ fi
 pyinstaller $(printf %s "$PLATFORM_OPTS")  \
   --add-data "./taskbar.png:." --add-data "../../web_ui/build:./web_ui/build" \
   --noconfirm --distpath=./desktop/build/dist --workpath=./desktop/build/work \
-  -n Kiln --specpath=./desktop/build \
+  -n Kiln --specpath=./desktop/build --hidden-import=tiktoken_ext.openai_public --hidden-import=tiktoken_ext \
   --paths=. ./desktop/desktop.py
